50a764b43c311146a1431f74e0023a98d060ef51
Daroshi11260 <rytaneja@gmail.com>
2023-07-30 20:01:26 -0400

    all deletions

--- lab1/src/main/java/module-info.java
+++
@@ -1,8 +0,0 @@
-module edu.ncssm.briansea.pset2 {
-    requires javafx.controls;
-
-    requires org.controlsfx.controls;
-    requires com.dlsc.formsfx;
-
-    exports com.cty.briansea.lab1;
-}
--- lab2/src/main/java/module-info.java
+++
@@ -1,9 +0,0 @@
-module edu.ncssm.briansea.pset3 {
-    requires javafx.controls;
-    requires javafx.fxml;
-
-    requires com.dlsc.formsfx;
-
-    opens com.cty.brainsea.lab2.pset3 to javafx.fxml;
-    exports com.cty.brainsea.lab2.pset3;
-}
